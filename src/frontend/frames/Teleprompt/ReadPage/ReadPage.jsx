66991deb60c90bc8c5f955cc170ff280811547b8
Jan Randa <jnranda@gmail.com>
2026-04-07 19:13:54 +0200

    ADD: better design and controls

--- src/frontend/frames/Teleprompt/ReadPage/ReadPage.jsx
+++ src/frontend/frames/Teleprompt/ReadPage/ReadPage.jsx
@@ -19,6 +19,27 @@ export const ReadPage = (props)=>{
     const { list, lastId } = prompts;
     const { text, duration, bpm } = list[currentId];
 
+    useEffect(_=>{
+        const keyboardControl = ev=>{
+            const toAction = {
+                "Escape": actions.stop,
+                "ArrowLeft": actions.prev,
+                "ArrowRight": actions.next,
+                "Backspace": actions.prev,
+                "Enter": actions.next,
+                " ": actions.repeat,
+                "Spacebar": actions.repeat,
+            };
+            const action = toAction[ev.key];
+            if (!action) { return; }
+            ev.preventDefault();
+            action();
+        };
+
+        document.addEventListener("keydown", keyboardControl);
+        return _=>document.removeEventListener("keydown", keyboardControl);
+    }, [actions, currentId, lastId]);
+
     useCountdown({
         countdownMs:3000,
         duration,
@@ -37,25 +58,27 @@ export const ReadPage = (props)=>{
         },
         onBeat:(prg, bs)=>{
             const ts = Math.floor(bs/10)/100;
-            prgEl.current.style.transition = `height ${ts*0.8}s ${ts*0.2}s ease-in-out`;
+            prgEl.current.style.transition = `height ${ts*1}s ${ts*0.0}s ease-in-out`;
             prgEl.current.style.height = ((1 - prg) * 100) + "%";
         }
     });
-    
+
     return (
         <div className="ReadPage">
-            <div className="progressBar" ref={prgEl}></div>
-
             <div className="grid">
-                <div className="text">{text}</div>
+                <div className="textBox">
+                    <div className="progressBar" ref={prgEl}></div>
+                    <div className="text">{text}</div>
+                    
+                </div>
                 <div className="status">
                     <div className="remainingTime" ref={remTimeEl}></div>
                     <div className="currentPrompt">{currentId+1}/{lastId+1}</div>
                 </div>
                 <div className="controls">
                     <Button className="previous" lock={currentId==0} onSubmit={actions.prev}>Previous</Button>
-                    <Button className="repeat" onSubmit={actions.repeat}>Repeat</Button>
                     <Button className="next" lock={currentId==lastId} onSubmit={actions.next}>Next</Button>
+                    <Button className="repeat" onSubmit={actions.repeat}>Repeat</Button>
                     <Button className="stop" onSubmit={actions.stop}>Stop</Button>
                 </div>
             </div>
